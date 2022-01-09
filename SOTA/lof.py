fb5050d9378951aef37ab35cbc30a3379e207c6e
chenbb <56836642+bin-soo@users.noreply.github.com>
2022-01-09 12:54:58 +0800

    Update lof.py

--- SOTA/lof.py
+++ SOTA/lof.py
@@ -1,38 +1,54 @@
-import os
-import sys 
-sys.path.append("..") 
-from LoadDownstreamData import GetTestSet,GetValSet,GetTrainSet
-
+from LoadDownstreamData import GetTrainSet,GetValSet,GetTestSet
 from pyod.models.lof import LOF
-from sklearn.metrics import roc_auc_score,roc_curve,auc
-import pickle
+from sklearn.metrics import roc_curve,auc,accuracy_score,precision_recall_curve
+import pickle,os
+path0 =os.path.abspath('..')
+path = os.getcwd()
 
-path = os.path.abspath(os.path.dirname(os.getcwd()))
-
-train_x,train_y = GetTrainSet(path)
-val_x,val_y = GetValSet(path)
-test_x,test_y = GetTestSet(path)
-
-epoch = 1
+train_x, train_y = GetTrainSet(path0)
+test_x, test_y  = GetTestSet(path0)
+test_y.tolist()
+epoch = 10
 clf_name = 'LOF'
-clf = LOF()
+clf = LOF(n_neighbors=8,contamination=0.4)
 
 for i in range(epoch):
-    clf.fit(train_x)
-    y_train_pred = clf.labels_
-    fpr, tpr, thersholds = roc_curve(train_y,y_train_pred)
+    y_pred = clf.fit_predict(test_x)
+    acc = accuracy_score(test_y, y_pred)
+    fpr, tpr, thersholds = roc_curve(test_y, y_pred)
     roc_auc = auc(fpr, tpr)
-    print(y_train_pred)
-    print(roc_auc)
+    prec, recall, thersholds2 = precision_recall_curve(test_y, y_pred)
+    prc_auc = auc(recall, prec)
+    print(y_pred)
+    print('acc: ', acc)
+    print('roc_auc: ', roc_auc)
+    print('prc_auc: ', prc_auc)
 
 #使用dump()将数据序列化到文件中
-fw = open(path+'/SOTA/LOF.txt','wb')
+fw = open(path+'/LOF.txt','wb')
 # Pickle dictionary using protocol 0.
 pickle.dump(clf, fw)
 fw.close()
 
-y_val_pred = clf.predict(val_x)
-fpr, tpr, thersholds = roc_curve(val_y,y_val_pred)
-roc_auc_val = auc(fpr, tpr)
-print(y_val_pred)
-print(roc_auc_val)
+'''
+setting n_neighbors = 8:
+[0 1 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+ 0 0 0 0 1 0 0 0 1 0 0 1 1 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0
+ 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0
+ 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 1
+ 0 0 0 0 0 0 0 1 0 0 0 0 0 0 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 0 0 0 0
+ 0 0 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 1]
+acc:  0.4876847290640394
+roc_auc:  0.501360544217687
+prc_auc:  0.5458128078817734
+
+setting n_neighbors = 8 and contamination = 0.4:
+[1 1 0 0 0 1 1 0 0 0 1 0 1 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 1 1 1 0 1 0
+ 1 0 0 1 1 0 0 1 1 1 1 1 1 1 0 0 0 0 0 0 0 1 0 0 1 1 0 0 0 0 0 0 1 1 0 0 0
+ 0 1 0 0 1 1 1 0 0 1 1 1 1 0 1 0 0 1 1 1 1 0 0 0 0 0 0 0 0 1 1 0 0 0 0 1 0
+ 0 0 0 0 0 1 0 0 0 1 1 1 0 1 1 1 0 0 0 0 0 0 0 0 1 1 0 0 0 0 0 1 0 0 0 0 1
+ 1 0 0 0 0 0 1 1 1 1 0 1 0 1 1 1 0 1 0 1 0 1 0 1 1 1 1 1 1 1 0 1 1 1 1 0 0
+ 0 1 1 1 1 0 0 0 0 0 0 0 0 0 0 0 0 1]
+acc:  0.4975369458128079
+roc_auc:  0.5010204081632653
+prc_auc:  0.6144316730523627'''
