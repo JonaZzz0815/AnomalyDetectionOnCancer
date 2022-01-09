a1e0b889478d80ebb6c2de9f8c1a0621ddd82a6f
chenbb <56836642+bin-soo@users.noreply.github.com>
2022-01-09 12:55:24 +0800

    Update pca.py

--- SOTA/pca.py
+++ SOTA/pca.py
@@ -1,38 +1,54 @@
-import os
-import sys 
-sys.path.append("..") 
-from LoadDownstreamData import GetTestSet,GetValSet,GetTrainSet
-
+from LoadDownstreamData import GetTrainSet,GetValSet,GetTestSet
 from pyod.models.pca import PCA
-from sklearn.metrics import roc_auc_score,roc_curve,auc
-import pickle
+from sklearn.metrics import roc_curve,auc,accuracy_score,precision_recall_curve
+import pickle,os
+path0 =os.path.abspath('..')
+path = os.getcwd()
 
-path = os.path.abspath(os.path.dirname(os.getcwd()))
-
-train_x,train_y = GetTrainSet(path)
-val_x,val_y = GetValSet(path)
-test_x,test_y = GetTestSet(path)
-
-epoch = 1
+train_x, train_y = GetTrainSet(path0)
+test_x, test_y  = GetTestSet(path0)
+test_y.tolist()
+epoch = 10
 clf_name = 'PCA'
-clf = PCA()
+clf = PCA(contamination=0.4)
 
 for i in range(epoch):
-    clf.fit(train_x)
-    y_train_pred = clf.labels_
-    fpr, tpr, thersholds = roc_curve(train_y,y_train_pred)
+    y_pred = clf.fit_predict(test_x)
+    acc = accuracy_score(test_y, y_pred)
+    fpr, tpr, thersholds = roc_curve(test_y, y_pred)
     roc_auc = auc(fpr, tpr)
-    print(y_train_pred)
-    print(roc_auc)
+    prec, recall, thersholds2 = precision_recall_curve(test_y, y_pred)
+    prc_auc = auc(recall, prec)
+    print(y_pred)
+    print('acc: ', acc)
+    print('roc_auc: ', roc_auc)
+    print('prc_auc: ', prc_auc)
+print(clf_name)
 
 #使用dump()将数据序列化到文件中
-fw = open(path+'/SOTA/PCA.txt','wb')
+fw = open(path+'/PCA.txt','wb')
 # Pickle dictionary using protocol 0.
 pickle.dump(clf, fw)
 fw.close()
+'''
+[0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 1 1 1 1
+ 1 0 0 0 0 0 0 0 1 0 1 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0
+ 0 0 0 0 0 1 1 1 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+ 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+ 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 1 1 1 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0
+ 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0]
+acc:  0.43842364532019706
+roc_auc:  0.4520408163265306
+prc_auc:  0.4152709359605911
 
-y_val_pred = clf.predict(val_x)
-fpr, tpr, thersholds = roc_curve(val_y,y_val_pred)
-roc_auc_val = auc(fpr, tpr)
-print(y_val_pred)
-print(roc_auc_val)
+setting contamination = 0.4:
+[0 1 1 0 1 0 1 1 1 1 1 1 0 1 1 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 1 1 1 1
+ 1 0 0 0 0 1 1 1 1 1 1 1 1 0 0 1 0 0 0 1 0 0 0 1 0 1 0 0 0 0 1 1 1 0 0 0 1
+ 1 1 0 0 1 1 1 1 0 0 0 0 0 0 1 0 0 1 0 1 0 0 1 1 0 0 0 0 0 1 1 0 0 0 0 1 0
+ 0 0 0 0 0 0 0 0 1 1 0 0 0 1 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1
+ 1 0 0 1 0 1 1 1 0 0 0 0 0 0 1 1 0 1 1 1 1 1 1 1 1 0 1 1 1 0 1 1 1 1 1 0 0
+ 0 0 0 1 1 0 0 0 0 0 0 0 0 0 0 0 0 1]
+acc:  0.4187192118226601
+roc_auc:  0.42210884353741496
+prc_auc:  0.5466581524052789
+'''
